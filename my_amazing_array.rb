4bad6d090e427a2e4b9adb64e8f22a2faad0f9cd
Serafeim Maroulis <seramak@gmail.com>
2014-04-20 22:41:44 +0100

    Everything up and working

--- my_amazing_array.rb
+++ my_amazing_array.rb
@@ -6,11 +6,41 @@ class Array
   #
   def sum
     # puts self.join(",")
+    sum = 0
+    self.each do |x|
+      sum += x
+    end
+    sum
   end
-
+#binding.pry
   # Returns the prime numbers in this array
   def primes
     # puts self.join(",")
+    primes = []
+    self.each do |x|
+      
+      if x==2
+        primes.push(x)
+      
+      elsif x%2!=0 && x!=1
+          bool = false
+          test_prime = (2...x).to_a
+
+          test_prime.each do |y|
+
+              if x%y==0
+                bool = true 
+                break
+              else
+                bool = false
+              end
+          end
+          if bool == false
+              primes.push(x)
+          end
+      end
+    end
+      primes
   end
 
   # Examine each pair in myself and swap them until all my
@@ -18,5 +48,35 @@ class Array
   #
   def bubble_sort
   	# puts self.join(",")
+     i = 0
+     temp = nil
+     bool = true
+
+  #binding.pry  
+    while bool #start of bool while
+      counter = 0
+      while i < self.length - 1
+        if self[i] > self[i+1]
+           temp = self[i]
+           self[i] = self[i+1]
+           self[i+1] = temp
+           counter += 1
+        end
+        i+=1
+      end
+
+      if counter == 0
+          bool = false 
+      else
+        i=0
+      end
+    end#end of while bool
+
+     self.each do |x|
+          print x
+     end 
   end
+
 end
+
+#binding.pry
